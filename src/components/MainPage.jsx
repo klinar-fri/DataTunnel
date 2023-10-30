9d03b69260f33b549b8f8414d8b92ab6ff3971de
klinar-fri <jk10104@student.uni-lj.si>
2023-10-30 22:04:56 +0100

    First ui changes

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,16 @@
+import "./MainPage.css";
+
+function MainPage() {
+    return(
+        <div className="frame">
+            <div className="firstPage"></div>
+            <div className="secondPageHeader">
+                <h1 className="question">Why use a vpn?</h1>
+            </div>
+            <div className="secondPage"></div>
+            <div className="thirdPageHeader"></div>
+        </div>
+    )
+}
+
+export default MainPage;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import "./NavBar.css";
+
+function NavBar() {
+    return(
+        <div className="wrapper">
+            
+        </div>
+    )
+}
+
+export default NavBar;
